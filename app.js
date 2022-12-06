b9d9abe9597cdde0f8b5eee0f85595e8f1749761
aliciasilverio <aliciaalicia2455@gmail.com>
2022-12-06 16:11:42 -0500

    math random

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-const colors = ["green", "red", "yellow"]
+const colors = ["green", "red", "yellow", "blue"]
 
 const btn = document.getElementById('btn');
 const color = document.querySelector(".color");
 
 // targeting the body
 btn.addEventListener('click', function(){
+    // getting random number between 0 and 3
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
 
-})
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber(){
+    return Math.random.();
+}
