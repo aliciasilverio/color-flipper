83424c6f07ea76fa596aef1cd3b014856b7590fc
aliciasilverio <aliciaalicia2455@gmail.com>
2022-12-06 15:59:19 -0500

    html setup and javascript

---
+++ app.js
@@ -0,0 +1,9 @@
+const colors = ["green", "red", "yellow"]
+
+const btn = document.getElementById('btn');
+const color = document.querySelector(".color");
+
+// targeting the body
+btn.addEventListener('click', function(){
+
+})
